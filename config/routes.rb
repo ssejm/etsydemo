3644e6edd27b2a8c8a5b68e2854541f0f556fa45
J M <partlysunny16@yahoo.com>
2015-04-18 17:03:20 -0700

    added seller page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :listings
-  get 'pages/about'
 
+  get 'pages/about'
   get 'pages/contact'
+  get 'seller' => "listings#seller"
 
   root 'listings#index'
 
